5cfa6902c12e3f615abc1da9caec45971f8499a5
Eymen Eid <eymeneid@gmail.com>
2024-05-17 23:59:06 +0300

    initial

--- hmsas/src/HealthcareProfessional.cpp
+++
@@ -1,8 +0,0 @@
-#include "HealthcareProfessional.h"
-
-// Constructor
-HealthcareProfessional::HealthcareProfessional(int id, const std::string& name, const std::string& contactInfo, const std::string& role)
-    : Person(id, name, contactInfo, role) {}
-
-// Destructor
-HealthcareProfessional::~HealthcareProfessional() {}
